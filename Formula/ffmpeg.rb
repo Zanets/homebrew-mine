bc7d3d09675d2a173f2131aae4b9f364b62a8ba5
Zanets <darg20127@gmail.com>
2018-11-07 00:46:36 +0800

    [feat][ffmpeg] use dmg

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -4,8 +4,8 @@ class Ffmpeg < Formula
   homepage "https://www.ffmpeg.org/"
 
   if OS.mac?
-    url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.7z"
-    sha256 "0a24ffa0fc221ad40a8304f52498fef2ea33e2d29d76415346c6269571177e9a"
+    url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.dmg"
+    sha256 "791469be9b985f408790dfe9fed64b11f81010e691109a6671c14558456be8ff"
   end
 
 
