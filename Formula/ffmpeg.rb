2980c7ea79a2c721073a2a63c46fd91a72118e07
Zanets <darg20127@gmail.com>
2018-11-07 13:58:41 +0800

    [fix][ffmpeg] add linux url

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -6,6 +6,9 @@ class Ffmpeg < Formula
   if OS.mac?
     url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.dmg"
     sha256 "791469be9b985f408790dfe9fed64b11f81010e691109a6671c14558456be8ff"
+  elsif OS.linux?
+    url "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz"
+    sha256 "db4753fc898c80276668368cd99f00e1b7980dcf20568b9c69b86c857646acb8"
   end
 
 
