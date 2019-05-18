9d5da2d65d568ade28b0a1d00c41ed856107e21b
Zanets <darg20127@gmail.com>
2019-05-19 02:34:12 +0800

    [ref] update version
    asd

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,23 +1,19 @@
 class Bat < Formula
-  version '0.8.0'
+  version '0.11.0'
   desc "A cat(1) clone with wings."
   homepage "https://github.com/sharkdp/bat"
 
   if OS.mac?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "aec82b058c6df5ab611883449093a179f49eac8b8f31aa26c47c4289f0f29c1b"
+    sha256 "bbe65ca55dc1d1dec2273cf8e5e7c2ac9f84c74548f13b947b573d00adf94dd5"
   elsif OS.linux?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "b0f74ea66180f67d34fcfea528d55309571abc5d19065b0dad4ef23356d80e95"
+    sha256 "96f10eb21eda3575dcef5250c602dbd9bb7237e63205a34afc347c6bd04a1cd6"
   end
 
 
   def install
     bin.install "bat"
     man1.install "bat.1"
-
-    bash_completion.install "autocomplete/bat.bash-completion"
-    fish_completion.install "autocomplete/bat.fish"
-    zsh_completion.install "autocomplete/_bat"
   end
 end
--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,18 +1,20 @@
 class Ffmpeg < Formula
-  version '4.1'
+  version '4.1.3'
   desc "A complete, cross-platform solution to record, convert and stream audio and video."
   homepage "https://www.ffmpeg.org/"
 
   if OS.mac?
-    url "https://evermeet.cx/ffmpeg/ffmpeg-#{version}.dmg"
-    sha256 "791469be9b985f408790dfe9fed64b11f81010e691109a6671c14558456be8ff"
+    url "https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-20190517-96c79d4-macos64-static.zip"
+    sha256 "56bccd1d960c20d2abcac80470710433cb6556cfe62b5495629e6d16c3726ec0"
   elsif OS.linux?
-    url "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz"
-    sha256 "db4753fc898c80276668368cd99f00e1b7980dcf20568b9c69b86c857646acb8"
+    url "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
+    sha256 "60a20667c2f30368a47f764830701818ce823888bfb8168623248dad2b38ba37<Paste>"
   end
 
 
   def install
-    bin.install "ffmpeg"
+    bin.install "bin/ffmpeg"
+	bin.install "bin/ffplay"
+	bin.install "bin/ffprobe"
   end
 end
