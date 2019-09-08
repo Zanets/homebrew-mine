e998ac6322d4fb7c8db6fe20fbcaceec1a3c067c
Zanets <darg20127@gmail.com>
2019-09-08 22:35:26 +0800

    [ref][bat] upgrade to 0.12.1

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,14 +1,14 @@
 class Bat < Formula
-  version '0.11.0'
+  version '0.12.1'
   desc "A cat(1) clone with wings."
   homepage "https://github.com/sharkdp/bat"
 
   if OS.mac?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "bbe65ca55dc1d1dec2273cf8e5e7c2ac9f84c74548f13b947b573d00adf94dd5"
+    sha256 "0d1b97d29de048fa888ebd57f52f36330d653dc29365f905a185b322a8f80a4d"
   elsif OS.linux?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "96f10eb21eda3575dcef5250c602dbd9bb7237e63205a34afc347c6bd04a1cd6"
+    sha256 "2709a2be8a40a5d267627729cd240df28167347dc202b8e76d14b05978ee1aaa"
   end
 
 
