16702ddccf302800f8c218f3046a0ef59424b150
Zanets <darg20127@gmail.com>
2019-10-05 21:40:51 +0800

    update annie to 0.9.5

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -1,14 +1,14 @@
 class Annie < Formula
-  version '0.9.4'
+  version '0.9.5'
   desc "Fast, simple and clean video downloader"
   homepage "https://github.com/iawia002/annie"
 
   if OS.mac?
     url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_macOS_64-bit.tar.gz"
-    sha256 "71192d49d6b2a4ac3e19fa51bb8dc4df1c0e73525fd80d40d811d62d84849ba1"
+    sha256 "457e119e1bc9f31d08e32b8a79a75508cd3091b0b5fbaa9cc6e2dff5c7183dcd"
   elsif OS.linux?
     url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_64-bit.tar.gz"
-    sha256 "49212895f9dc59e2e2f90f3679ad0eea8ff778bf9eb9531190158c869cd4849e"
+    sha256 "0a16c6b8e2b04268d0b23cb3e55d5854cc4bccab658b254942a0ae3504dd0c7c"
   end
 
 
