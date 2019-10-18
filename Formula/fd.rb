8d30a02b06fb02f6cb9142633a6ec76e8b5a0576
Zanets <darg20127@gmail.com>
2019-10-19 01:36:13 +0800

    update fd to 7.4.0

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,14 +1,14 @@
 class Fd < Formula
-  version '7.3.0'
+  version '7.4.0'
   desc "A simple, fast and user-friendly alternative to 'find'."
   homepage "https://github.com/sharkdp/fd"
 
   if OS.mac?
     url "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "c37cd27c3e3f31dc0c676854cb9dd20470a2815a3a37d012d52e93c0ea9432a8"
+    sha256 "07200f046915c82c7ca9929286e042ede7d4f4639b11787714578e585335bf88"
   elsif OS.linux?
     url "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "fd49c1e181b94a10f1f8a2325b51418a3978c752595d8192b386ccad67a7dc0b"
+    sha256 "5bb566d9a5b9190ba8da94bd0ef1d609aab82433c0988613192a7bbb4aae395c"
   end
 
   def install
