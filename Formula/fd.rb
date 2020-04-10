55dba6fc543e5c695223a979d31c86ad3ec8af72
Zane Tsai <zane_tsai@arcadyan.com>
2020-04-10 17:16:10 +0800

    [feat] upgrade fd to 7.5.0

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -1,14 +1,14 @@
 class Fd < Formula
-  version '7.4.0'
+  version '7.5.0'
   desc "A simple, fast and user-friendly alternative to 'find'."
   homepage "https://github.com/sharkdp/fd"
 
   if OS.mac?
     url "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "07200f046915c82c7ca9929286e042ede7d4f4639b11787714578e585335bf88"
+    sha256 "cbc9c818d8769d54c77a8bb9080d5c45880280b63ea566cfe4d10284983d0935"
   elsif OS.linux?
     url "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "5bb566d9a5b9190ba8da94bd0ef1d609aab82433c0988613192a7bbb4aae395c"
+    sha256 "e2ad00c5b369470fb6a7b150b3d2d53b9f3f3bcbb39c7f82779902a4076ea05c"
   end
 
   def install
