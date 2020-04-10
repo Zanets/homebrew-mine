341f391e14ef2342aea22528bf942d309e8dcf59
Zane Tsai <zane_tsai@arcadyan.com>
2020-04-10 17:20:49 +0800

    [bat][feat] update bat to 0.13.0

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,14 +1,14 @@
 class Bat < Formula
-  version '0.12.1'
+  version '0.13.0'
   desc "A cat(1) clone with wings."
   homepage "https://github.com/sharkdp/bat"
 
   if OS.mac?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "0d1b97d29de048fa888ebd57f52f36330d653dc29365f905a185b322a8f80a4d"
+    sha256 "e6b9b00ac30c2920f1a1fb2be57224c314519d55559f9017125671b6368286cd"
   elsif OS.linux?
     url "https://github.com/sharkdp/bat/releases/download/v#{version}/bat-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "2709a2be8a40a5d267627729cd240df28167347dc202b8e76d14b05978ee1aaa"
+    sha256 "4a1650c2b82eaa92f08f47ce0d0eb72946d3a92ec748672fab2c94920bef7818"
   end
 
 
