fe59293131ad6a2add54d413e5006ddc19f5b491
Zane Tsai <zane_tsai@arcadyan.com>
2020-04-10 17:20:28 +0800

    [annie][feat] upgrade to 0.9.8

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -1,14 +1,14 @@
 class Annie < Formula
-  version '0.9.5'
+  version '0.9.8'
   desc "Fast, simple and clean video downloader"
   homepage "https://github.com/iawia002/annie"
 
   if OS.mac?
     url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_macOS_64-bit.tar.gz"
-    sha256 "457e119e1bc9f31d08e32b8a79a75508cd3091b0b5fbaa9cc6e2dff5c7183dcd"
+    sha256 "1921e50048f54b482d4e983ab4e4851072760375271827327fb2df86f6860772"
   elsif OS.linux?
     url "https://github.com/iawia002/annie/releases/download/#{version}/annie_#{version}_Linux_64-bit.tar.gz"
-    sha256 "0a16c6b8e2b04268d0b23cb3e55d5854cc4bccab658b254942a0ae3504dd0c7c"
+    sha256 "90b530e81e9414d5418d05c48815e238ea806a4de156cbdd01349a3f0965d184"
   end
 
 
