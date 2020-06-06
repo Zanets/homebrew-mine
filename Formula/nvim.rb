ff681fdc419a45bf9f919a707273498f4166e18e
Zanets <darg20127@gmail.com>
2020-06-06 21:37:49 +0800

    [nvim] upgrade to 0.4.3

--- Formula/nvim.rb
+++ Formula/nvim.rb
@@ -1,14 +1,14 @@
 class Nvim < Formula
-  version '0.4.2'
+  version '0.4.3'
   desc "Vim-fork focused on extensibility and usability."
   homepage "https://github.com/neovim/neovim"
 
   if OS.mac?
     url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-macos.tar.gz"
-    sha256 "bf701dfaa456d0e82f7c3fb885861af1282438cc9622853ca76b22d90d0a2516"
+    sha256 "2354a60af47a61f54ad1fc115587af8db96dfbe8c8955c516af591d398134a2d"
   elsif OS.linux?
     url "https://github.com/neovim/neovim/releases/download/v#{version}/nvim-linux64.tar.gz"
-    sha256 "254a889274a758ce71c0ae619410616c5e26aeece3e9b6403779c9b7625b23e6"
+    sha256 "7b9e10c6e04434824637beb65a7593d879716dc28c1833a261987b8deed38c6b"
   end
 
 
