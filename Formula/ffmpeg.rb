1577dc5104d729fa08922d4df325fa1bb9711d6a
Zanets <darg20127@gmail.com>
2020-07-16 23:43:52 +0800

    [ffmpeg] upgrade to 4.3.1

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -1,14 +1,14 @@
 class Ffmpeg < Formula
-  version '4.1.3'
+  version '4.3.1'
   desc "A complete, cross-platform solution to record, convert and stream audio and video."
   homepage "https://www.ffmpeg.org/"
 
   if OS.mac?
     url "https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-#{version}-macos64-static.zip"
-    sha256 "56bccd1d960c20d2abcac80470710433cb6556cfe62b5495629e6d16c3726ec0"
+    sha256 "ad768b3faa527d1a95eb157663f22a4d831ce9a307095176a67402f44d8c4575"
   elsif OS.linux?
     url "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
-    sha256 "60a20667c2f30368a47f764830701818ce823888bfb8168623248dad2b38ba37<Paste>"
+    sha256 "0c110ef554aa22053f1aa12e5bb49204754a88d50d66bb6ac3e81d2aa85a8a06"
   end
 
 
