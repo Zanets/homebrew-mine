df3c43b64d7696074239de01dfa069dbc939c6b7
Zanets <darg20127@gmail.com>
2020-07-16 23:49:36 +0800

    .

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -5,7 +5,7 @@ class Ffmpeg < Formula
 
   if OS.mac?
     url "https://evermeet.cx/pub/ffmpeg/ffmpeg-#{version}.zip"
-    sha256 "ad768b3faa527d1a95eb157663f22a4d831ce9a307095176a67402f44d8c4575"
+    sha256 "8f3e9fca63a76d9f69bc60daf5aa8b524178de0e425452a2ecd872a9fd2eb9f6"
   elsif OS.linux?
     url "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
     sha256 "0c110ef554aa22053f1aa12e5bb49204754a88d50d66bb6ac3e81d2aa85a8a06"
